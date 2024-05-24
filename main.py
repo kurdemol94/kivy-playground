c84bf401f5948d2d6aed1579c259cdce1aa675ca
Maciek <kurdemol94@gmail.com>
2024-05-24 21:51:43 +0200

    add input, check different gh action config

--- main.py
+++ main.py
@@ -4,17 +4,29 @@ from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.uix.scatter import Scatter
 from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.textinput import TextInput
 
 
 class TutorialApp(App):
     def build(self):
-        f = FloatLayout();
-        s = Scatter()        
-        b = Label(text = "Hello")
+        defaultText = 'enter your text here'
+        floatLayout = FloatLayout()
+        textInput = TextInput(font_size=150, size_hint_y=None, height=200, text=defaultText)
+        boxLayout = BoxLayout(orientation='vertical')
+        scatter = Scatter()        
+        label = Label(text=defaultText)
 
-        f.add_widget(s)
-        s.add_widget(b)
-        return f
+        textInput.bind(text=label.setter('text'))
+
+        floatLayout.add_widget(scatter)
+        scatter.add_widget(label)
+
+
+        boxLayout.add_widget(textInput)
+        boxLayout.add_widget(floatLayout)
+        return boxLayout
+    
 
 if __name__ == "__main__":
         TutorialApp().run()
